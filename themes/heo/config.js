e399fc0821b4e2a2b021ffc5b20335e459949de2
enzohahalife <124018333+enzohahalife@users.noreply.github.com>
2024-12-11 19:17:52 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -18,14 +18,14 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享产品',
-  HEO_HERO_TITLE_2: '与思维认知',
+  HEO_HERO_TITLE_1: '分享知识',
+  HEO_HERO_TITLE_2: '与思维成长',
   HEO_HERO_TITLE_3: 'Duality Realm lab',
   HEO_HERO_TITLE_4: '新版上线',
   HEO_HERO_TITLE_5: '二象界实验室',
   HEO_HERO_TITLE_LINK: 'https://www.drlab.top',
   // 英雄区遮罩文字
-  HEO_HERO_COVER_TITLE: '随便逛逛',
+  HEO_HERO_COVER_TITLE: '随便闲逛',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
