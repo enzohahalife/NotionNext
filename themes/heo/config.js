254da91a78310565d623b3633bcdb0238f3c1295
enzohahalife <124018333+enzohahalife@users.noreply.github.com>
2024-12-11 19:15:02 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -18,11 +18,11 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
+  HEO_HERO_TITLE_1: '分享产品',
   HEO_HERO_TITLE_2: '与思维认知',
   HEO_HERO_TITLE_3: 'Duality Realm lab',
   HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
+  HEO_HERO_TITLE_5: '二象界实验室',
   HEO_HERO_TITLE_LINK: 'https://www.drlab.top',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
